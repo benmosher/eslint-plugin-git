e847a9e118630e66e897b844687b341b64062255
Ben Mosher <me@benmosher.com>
2016-04-05 07:05:45 -0400

    full test coverage

--- test/rules/no-untracked.js
+++ test/rules/no-untracked.js
@@ -17,13 +17,14 @@ function test(t) {
   }, t)
 }
 
+const cacheTestFile = path.resolve('./test/files/cache-correctness.js')
+
 describe("main tests", function () {
   const files = [
     path.resolve('./test/files/ignored.js'),
     path.resolve('./test/files/untracked.js'),
   ]
 
-  const cacheTestFile = path.resolve('./test/files/cache-correctness.js')
 
   before("create untracked files", function () {
     files.forEach(f => touch.sync(f))
@@ -42,7 +43,7 @@ describe("main tests", function () {
       // core modules are fine
       test({ code: 'import "fs"' }),
       // out of scope`
-      test({ code: 'import "/some/root/fake/thing"' }),
+      test({ code: 'import "/bin/sh"' }),
       // this doesn't exist (yet)
       test({ code: `import "${cacheTestFile}"`}),
     ],
@@ -54,22 +55,49 @@ describe("main tests", function () {
       }),
     ],
   })
+})
 
-  describe("cache correctness", function () {
+describe("cache correctness", function () {
 
-    const testDesc = test({
-      code: `import "${cacheTestFile}"`,
-      settings: { 'import/cache': { lifetime: 1 } },
-    })
+  const testDesc = test({
+    code: `import "${cacheTestFile}"`,
+    settings: { 'import/cache': { lifetime: 1 } },
+  })
 
-    before("touch file", () => touch.sync(cacheTestFile))
-    before("wait for cache lifetime to expire", done => setTimeout(done, 1100))
+  before("touch file", () => touch.sync(cacheTestFile))
+  before("wait for cache lifetime to expire", done => setTimeout(done, 1100))
 
-    after("rm file", () => fs.unlinkSync(cacheTestFile))
+  after("rm file", () => fs.unlinkSync(cacheTestFile))
 
-    // actual test
-    ruleTester.run('tests', rule,
-      { valid: [], invalid: [ Object.assign({ errors: 1}, testDesc) ] })
+  // actual test
+  ruleTester.run('tests', rule, {
+    valid: [],
+    invalid: [ Object.assign({ errors: 1}, testDesc) ] ,
+  })
 
+})
+
+describe("no untracked files", function () {
+  const settings = { 'import/cache': { lifetime: 0 } }
+
+  ruleTester.run('tests', rule, {
+    valid: [
+      // this exists and is tracked
+      test({
+        code: 'import "./tracked"',
+        settings,
+      }),
+      // this no longer exists
+      test({
+        code: 'import "./untracked"',
+        settings,
+      }),
+      // this still exists and is gitignore'd
+      test({
+        code: 'import "./ignored"',
+        settings,
+      }),
+    ],
+    invalid: [],
   })
 })
